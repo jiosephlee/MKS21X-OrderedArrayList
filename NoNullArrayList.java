df9052ef2d834bb4419e34341453b099e6e6128d
John Dogit config --global user.name John <jlee01@stuy.edu>
2018-11-01 14:51:39 -0400

    end of period

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,3 +1,36 @@
-NoNullArrayList<T> extends ArrayList<T>{
-  
+import java.util.ArrayList;
+public class NoNullArrayList<T> extends ArrayList<T>{
+  public NoNullArrayList(){
+    super();
+  }
+
+  public NoNullArrayList(int start){
+    super(start);
+  }
+
+  public T set(int index, T value){
+    if (value == null){
+      throw new IndexOutOfBoundsException("Error: Argument cannot be null");
+    } else {
+    super.set(value);
+    }
+  }
+
+  public boolean add(T value){
+    if (value == null){
+      throw new IndexOutOfBoundsException("Error: Argument cannot be null");
+    } else {
+      super.add(value);
+    }
+  }
+
+  public void add(int index, T value){
+    if (value == null){
+      throw new IndexOutOfBoundsException("Error: Argument cannot be null");
+    } else {
+      super.add(index, value);
+    }
+  }
+
+
 }
