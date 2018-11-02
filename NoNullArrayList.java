a225b69dcb3662136495822b8383b099ebad9aed
Joseph Lee <jlee01@stuy.edu>
2018-11-01 21:57:04 -0400

    finished nonull and created skeleton for ordered

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -9,11 +9,13 @@ public class NoNullArrayList<T> extends ArrayList<T>{
   }
 
   public T set(int index, T value){
+    T output = get(index);
     if (value == null){
       throw new IndexOutOfBoundsException("Error: Argument cannot be null");
     } else {
-    super.set(value);
+    super.set(index, value);
     }
+    return output;
   }
 
   public boolean add(T value){
@@ -22,6 +24,7 @@ public class NoNullArrayList<T> extends ArrayList<T>{
     } else {
       super.add(value);
     }
+    return true;
   }
 
   public void add(int index, T value){
---
+++ OrderedArrayList.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>{
+  public OrderedArrayList(){
+    super();
+  }
+
+  public OrderedArrayList(int start){
+    super(start);
+  }
+
+  public T set(int index, T value){
+
+  }
+
+  public boolean add(T value){
+
+  }
+
+  public void add(int index, T value){
+
+  }
+}
